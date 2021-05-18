c4f210d8407d714967d4dba5a92ffbcd971f996a
MuvvaSaiKarthik <54205485+MuvvaSaiKarthik@users.noreply.github.com>
2021-05-18 20:27:09 +0530

    Add files via upload

--- 01 Lesson/js/custom.js
+++ 01 Lesson/js/custom.js
@@ -1,1 +1,60 @@
+/*var a = 5;
+var b = "5";
+if(a===b) {
+    console.log("true")
+} else{
+    console.log("false")
+}*/
 
+/*var a = 0;
+while(a<10) {
+    console.log(a + " Hello World");
+    a = a+1;
+}*/
+
+/*var a = 0;
+do{
+    console.log(a + " Hello World");
+    a = a+1;
+}while(a<10)*/
+
+/*for(i=0;i<10;i++) {
+    console.log(i + " Hello World");
+    if(i==6) {
+        console.log("we have got 6")
+        continue;
+    }
+}*/
+/*function myFunction(x,y,z) {
+    console.log(x+y+z);
+}*/
+
+/*function loopFrom(x,y,z) {
+    for(i=x;i<y;i++){
+        console.log(i + " Hello World");
+        if(i==z){
+            console.log("Hi there");
+            continue;
+        }
+    }
+}*/
+/*var a = [10,30,40,20,50];
+var arr = a.sort();
+console.log(arr);*/
+/*var a = [10,30,40,20,50];
+var arr = a.reverse();
+console.log(arr);*/
+/*var a = [10,30,40,20,50];
+var arr = a.join();
+console.log(arr);*/
+
+"use strict";
+var something = "Hello World";
+var person = {
+    u_name:1,
+    u_another_name:2
+}
+function myFunction(a,b,c) {
+    return a+b+c;
+}
+document.getElementById("heading").innerHTML = something;
